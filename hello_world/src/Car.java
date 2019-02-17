4a7cf4f2286f62be39b37905e37b3819a9fb0403
Bret <Burhooo@gmail.com>
2019-02-17 14:54:46 -0600

    Added Car constructor and created 2 cars

--- hello_world/src/Car.java
+++ hello_world/src/Car.java
@@ -2,9 +2,18 @@ import java.awt.*;
 
 public class Car {
 
-    int averageMilesPerGallon;
+    double averageMilesPerGallon;
     String licensePlate;
     Color paintColor;
     boolean areTailightsWorking;
 
+    public Car(double inputAverageMPG,
+               String inputLicensePlate,
+               Color inputPaintColor,
+               boolean inputAreTailightsWorking) {
+        this.averageMilesPerGallon = inputAverageMPG;
+        this.licensePlate = inputLicensePlate;
+        this.paintColor = inputPaintColor;
+        this.areTailightsWorking = inputAreTailightsWorking;
+    }
 }
--- hello_world/src/Main.java
+++ hello_world/src/Main.java
@@ -1,7 +1,19 @@
+import java.awt.*;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Bret");
+
+        Car myCar = new Car( 25.5,
+                "1BC32E",
+                Color.BLUE,
+                true
+                );
+        Car bretCar = new Car(23.2,
+                "324GBC",
+                Color.BLACK,
+                false);
     }
 }
